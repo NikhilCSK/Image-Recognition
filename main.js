c640c0cca0052ca7bff3227b547ca86e614a82ab
NikhilCSK <102224467+NikhilCSK@users.noreply.github.com>
2022-11-08 19:18:55 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+Webcam.set({
+
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality: 90
+
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+
+    Webcam.snap(function (data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+
+}
+
+
+console.log('ml5.version - ',ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/P7IF3-ftB/model.json', modelLoaded);
+
+function modelLoaded()
+{
+
+    console.log("model loaded");
+
+}
+
+    function check()
+    {
+
+        img = document.getElementById('captured_image');
+        classifier.classify(img, gotResult);
+
+    }
+
+    function gotResult(error, results)
+    {
+
+        if (error){
+
+            console.error(error);
+
+        }
+
+        else{
+
+            console.log(results);
+            document.getElementById("result_object_name").innerHTML = results[0].label;
+            x = results[0].confidence.toFixed(2);
+            document.getElementById("result_object_accuracy").innerHTML = x*100 + " % ";
+
+
+        }
+
+    }
